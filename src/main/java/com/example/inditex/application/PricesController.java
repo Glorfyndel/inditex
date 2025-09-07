16162ea3d860141c0ec80965b3d49a1a42dbc0bc
Bruno Mendez de Vigo Castillo <bruno.mendez-de-vigo@basf.com>
2025-09-07 17:50:29 +0200

    master - Add swagger.

--- src/main/java/com/example/inditex/application/PricesController.java
+++ src/main/java/com/example/inditex/application/PricesController.java
@@ -2,9 +2,18 @@ package com.example.inditex.application;
 
 import com.example.inditex.domain.ProductPrice;
 import com.example.inditex.domain.ProductPriceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ProblemDetail;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
+@Tag(name = "Price", description = "API for Inditex product prices")
 @RestController
 @RequestMapping("/inditex")
 @RequiredArgsConstructor
@@ -20,10 +30,16 @@ import java.time.LocalDateTime;
 public class PricesController {
     private final ProductPriceService productPriceService;
 
+    @Operation(summary = "Get price information based on brand, product and date")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json", schema = @Schema(implementation = PriceResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomRestExeptionHandler.ConstraintsErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Price not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @GetMapping("/prices")
-    public PriceResponse getPrices(@RequestParam @NotNull @Min(0) long brandId,
-                                   @RequestParam @NotNull @Min(0) long productId,
-                                   @RequestParam LocalDateTime applicationDate) {
+    public PriceResponse getPrices(@Parameter(required = true, example = "1") @RequestParam @NotNull @Min(0) long brandId,
+                                   @Parameter(required = true, example = "35455") @RequestParam @NotNull @Min(0) long productId,
+                                   @Parameter(required = true, example = "2020-06-14T10:00:00") @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime applicationDate) {
         final ProductPrice productPrice = productPriceService.getProductPrice(brandId, productId, applicationDate);
 
         final PriceResponse priceResponse = PriceResponse.builder()
--- src/main/java/com/example/inditex/domain/ProductPrice.java
+++ src/main/java/com/example/inditex/domain/ProductPrice.java
@@ -1,6 +1,7 @@
 package com.example.inditex.domain;
 
 import lombok.Builder;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -10,7 +11,9 @@ public record ProductPrice(
         long brandId,
         long productId,
         int priceList,
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
         LocalDateTime startDate,
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
         LocalDateTime endDate,
         BigDecimal price,
         int priority,
