d90a4838e1a617dd90d2884d984b415310f68852
Bruno Mendez de Vigo Castillo <bruno.mendez-de-vigo@basf.com>
2025-09-09 10:02:59 +0200

    master - Add github build workflow/pipepline.

--- src/main/java/com/example/inditex/commons/Todo.java
+++ src/main/java/com/example/inditex/commons/Todo.java
@@ -3,6 +3,12 @@ package com.example.inditex.commons;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+/**
+ * Utility class for marking unimplemented code during development.
+ * Use Todo.TODO() as a placeholder for methods not yet implemented.
+ *
+ * @deprecated Not intended for production use. Remove or replace before release.
+ */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Todo {
     public static <T> T TODO() {
