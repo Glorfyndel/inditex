eb492aa2b5ab98075cba5339648e241c60abb0b4
Bruno Mendez de Vigo Castillo <bruno.mendez-de-vigo@basf.com>
2025-09-11 15:11:08 +0200

    master - Refactor InditexIntegrationTest with parametrized test.

--- src/test/java/com/example/inditex/InditexIntegrationTest.java
+++ src/test/java/com/example/inditex/InditexIntegrationTest.java
@@ -4,8 +4,8 @@ import com.example.inditex.application.PriceResponse;
 import com.example.inditex.domain.Currency;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Configuration;
@@ -49,116 +49,34 @@ public class InditexIntegrationTest {
     }
 
     @DisplayName("Consultar precio de producto '1-35455' :")
-    @Nested
-    class ConsultarPecio {
-        @Test
-        @DisplayName("14-06-2020T10:00 -> tarifa 1")
-        void prueba1() {
-            final PriceResponse priceResponse = restClient.get()
-                    .uri("/inditex/prices?brandId={brandId}&productId={productId}&applicationDate={applicationDate}",
-                            1, 35455, LocalDateTime.parse("2020-06-14T10:00:00"))
-                    .retrieve()
-                    .toEntity(PriceResponse.class)
-                    .getBody();
+    @ParameterizedTest(name = "A las {2} -> tarifa {3}")
+    @CsvSource(delimiter = '|', value = {
+            /* brandId | productId | applicationDate     | priceList | startDate           | endDate             | price  | currency */
+            "  1       | 35455     | 2020-06-14T10:00:00 | 1         | 2020-06-14T00:00:00 | 2020-12-31T23:59:59 | 35.50  | EUR      ",
+            "  1       | 35455     | 2020-06-14T16:00:00 | 2         | 2020-06-14T15:00:00 | 2020-06-14T18:30:00 | 25.45  | EUR      ",
+            "  1       | 35455     | 2020-06-14T21:00:00 | 1         | 2020-06-14T00:00:00 | 2020-12-31T23:59:59 | 35.50  | EUR      ",
+            "  1       | 35455     | 2020-06-15T10:00:00 | 3         | 2020-06-15T00:00:00 | 2020-06-15T11:00:00 | 30.50  | EUR      ",
+            "  1       | 35455     | 2020-06-16T21:00:00 | 4         | 2020-06-15T16:00:00 | 2020-12-31T23:59:59 | 38.95  | EUR      "
+    })
+    void consultarPrecioProducto(long brandId, long productId, String applicationDate, int priceList, String startDate,
+                                 String endDate, String price, String currency) {
 
-            assertThat(priceResponse)
-                    .isEqualTo(PriceResponse.builder()
-                            .brandId(1L)
-                            .productId(35455L)
-                            .priceList(1)
-                            .startDate(LocalDateTime.parse("2020-06-14T00:00:00"))
-                            .endDate(LocalDateTime.parse("2020-12-31T23:59:59"))
-                            .price(new BigDecimal("35.50"))
-                            .currency(Currency.EUR)
-                            .build());
-        }
+        final PriceResponse priceResponse = restClient.get()
+                .uri("/inditex/prices?brandId={brandId}&productId={productId}&applicationDate={applicationDate}",
+                        brandId, productId, LocalDateTime.parse(applicationDate))
+                .retrieve()
+                .toEntity(PriceResponse.class)
+                .getBody();
 
-        @Test
-        @DisplayName("14-06-2020T16:00 -> tarifa 2")
-        void prueba2() {
-            final PriceResponse priceResponse = restClient.get()
-                    .uri("/inditex/prices?brandId={brandId}&productId={productId}&applicationDate={applicationDate}",
-                            1, 35455, LocalDateTime.parse("2020-06-14T16:00:00"))
-                    .retrieve()
-                    .toEntity(PriceResponse.class)
-                    .getBody();
-
-            assertThat(priceResponse)
-                    .isEqualTo(PriceResponse.builder()
-                            .brandId(1L)
-                            .productId(35455L)
-                            .priceList(2)
-                            .startDate(LocalDateTime.parse("2020-06-14T15:00:00"))
-                            .endDate(LocalDateTime.parse("2020-06-14T18:30:00"))
-                            .price(new BigDecimal("25.45"))
-                            .currency(Currency.EUR)
-                            .build());
-        }
-
-        @Test
-        @DisplayName("14-06-2020T21:00 -> tarifa 1")
-        void prueba3() {
-            final PriceResponse priceResponse = restClient.get()
-                    .uri("/inditex/prices?brandId={brandId}&productId={productId}&applicationDate={applicationDate}",
-                            1, 35455, LocalDateTime.parse("2020-06-14T21:00:00"))
-                    .retrieve()
-                    .toEntity(PriceResponse.class)
-                    .getBody();
-
-            assertThat(priceResponse)
-                    .isEqualTo(PriceResponse.builder()
-                            .brandId(1L)
-                            .productId(35455L)
-                            .priceList(1)
-                            .startDate(LocalDateTime.parse("2020-06-14T00:00:00"))
-                            .endDate(LocalDateTime.parse("2020-12-31T23:59:59"))
-                            .price(new BigDecimal("35.50"))
-                            .currency(Currency.EUR)
-                            .build());
-        }
-
-        @Test
-        @DisplayName("15-06-2020T10:00 -> tarifa 3")
-        void prueba4() {
-            final PriceResponse priceResponse = restClient.get()
-                    .uri("/inditex/prices?brandId={brandId}&productId={productId}&applicationDate={applicationDate}",
-                            1, 35455, LocalDateTime.parse("2020-06-15T10:00:00"))
-                    .retrieve()
-                    .toEntity(PriceResponse.class)
-                    .getBody();
-
-            assertThat(priceResponse)
-                    .isEqualTo(PriceResponse.builder()
-                            .brandId(1L)
-                            .productId(35455L)
-                            .priceList(3)
-                            .startDate(LocalDateTime.parse("2020-06-15T00:00:00"))
-                            .endDate(LocalDateTime.parse("2020-06-15T11:00:00"))
-                            .price(new BigDecimal("30.50"))
-                            .currency(Currency.EUR)
-                            .build());
-        }
-
-        @Test
-        @DisplayName("16-06-2020T21:00 -> tarifa 4")
-        void prueba5() {
-            final PriceResponse priceResponse = restClient.get()
-                    .uri("/inditex/prices?brandId={brandId}&productId={productId}&applicationDate={applicationDate}",
-                            1, 35455, LocalDateTime.parse("2020-06-16T21:00:00"))
-                    .retrieve()
-                    .toEntity(PriceResponse.class)
-                    .getBody();
-
-            assertThat(priceResponse)
-                    .isEqualTo(PriceResponse.builder()
-                            .brandId(1L)
-                            .productId(35455L)
-                            .priceList(4)
-                            .startDate(LocalDateTime.parse("2020-06-15T16:00:00"))
-                            .endDate(LocalDateTime.parse("2020-12-31T23:59:59"))
-                            .price(new BigDecimal("38.95"))
-                            .currency(Currency.EUR)
-                            .build());
-        }
+        assertThat(priceResponse)
+                .isEqualTo(PriceResponse.builder()
+                        .brandId(brandId)
+                        .productId(productId)
+                        .priceList(priceList)
+                        .startDate(LocalDateTime.parse(startDate))
+                        .endDate(LocalDateTime.parse(endDate))
+                        .price(new BigDecimal(price))
+                        .currency(Currency.valueOf(currency))
+                        .build());
     }
 }
